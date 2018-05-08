189fad78fed6c05b36622dac136bac04002473a8
ravindra rawat <ravinrawat.wd@gmail.com>
2018-05-08 03:12:30 +0000

    mongodb connected

--- server.js
+++ server.js
@@ -1,12 +1,27 @@
 const express = require("express");
 const app = express();
+const mongoose = require("mongoose");
 
 const port = process.env.PORT || 5000;
+
+// db config
+const db = require("./config/key").mongoURI;
+
+// cONNECT TO MONGODB
+mongoose
+  .connect(db)
+  .then(() => {
+    console.log(" Mongodb connected...");
+  })
+  .catch(err => {
+    console.log(err);
+  });
 
 app.get("/", (req, res) => {
   res.send("Hello World");
 });
 
+//Server listening....
 app.listen(port, () => {
   console.log(`Server running on port ${port} ...`);
 });

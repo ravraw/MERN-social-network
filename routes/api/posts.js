47c588877a71a330bd62818a12062288cc8eef2a
ravindra rawat <ravinrawat.wd@gmail.com>
2018-05-12 23:37:17 +0000

    added routes to delete grt all posts , det a single post and delete a single post

--- routes/api/posts.js
+++ routes/api/posts.js
@@ -5,6 +5,8 @@ const passport = require("passport");
 
 // Post  model
 const Post = require("../../models/Posts");
+// User  model
+//const User = require("../../models/User");
 
 // Validation
 
@@ -17,6 +19,25 @@ router.get("/test", (req, res) => {
   res.status(200).json({
     message: "Handling /posts route"
   });
+});
+
+// @route   GET api/posts
+// @desc    get posts
+// @access  Public
+router.get("/", (req, res) => {
+  Post.find()
+    .sort({ data: -1 })
+    .then(posts => res.json(posts))
+    .catch(err => res.status(404).json({ noPosts: "No posts found !" }));
+});
+
+// @route   GET api/posts/:id
+// @desc    get a single post by id
+// @access  Public
+router.get("/:id", (req, res) => {
+  Post.findById(req.params.id)
+    .then(post => res.json(post))
+    .catch(err => res.status(404).json({ noPost: "No post found" }));
 });
 
 // @route   POST api/posts
@@ -42,6 +63,29 @@ router.post(
       user: req.user.id
     });
     newPost.save().then(post => res.json(post));
+  }
+);
+
+// @route   DELETE api/posts/:id
+// @desc   Delete a single post
+// @access  Private
+
+router.delete(
+  "/:id",
+  passport.authenticate("jwt", { session: false }),
+  (req, res) => {
+    Post.findById(req.params.id)
+      .then(post => {
+        // check for post owner]
+        if (post.user.toString() !== req.user.id) {
+          return res
+            .status(401)
+            .json({ notauthorized: "User is not authorized to delete" });
+        }
+        //Delete
+        post.remove().then(() => res.json({ success: true }));
+      })
+      .catch(err => res.status(404).json({ postnotfound: "No" }));
   }
 );
 
